0ac4fb79084e46d6b12e731f3925b069d84a18a6
William Mitre Filho <willmitre@gmail.com>
2026-02-20 18:37:18 -0300

    This shall be the most practical way of UI-ing in SVG

---
+++ soum.js
@@ -0,0 +1,126 @@
+const parser = new DOMParser()
+Object.defineProperty(HTMLSpanElement.prototype, "innerText", {
+	set:function(newValue){
+		this.innerHTML = newValue
+		this.hasChanged(newValue)
+	},
+})
+
+Element.prototype.hasChanged = function(){
+}
+
+class Soum{
+	static documentChanged(element){
+		console.log(element, "has changed")
+	}
+	static removeAllFrom(element){
+		while(element.childNodes.length > 0)
+			element.removeChild(element.childNodes[0])
+	}
+	static newId(){
+		return `o${++Soum.counter}`
+	}
+	constructor(value, soumId = Soum.newId()){
+		this.value = value
+		this.soumId = soumId
+	}
+}
+
+Soum.counter = 0
+
+class SoumElement{
+
+	constructor(name, soumId = Soum.newId()){
+		this._element = document.createElement(name)
+		this._element.setAttribute("id", soumId)
+		this.soumId = soumId
+	}
+
+	redraw(){
+		if(this.element.parentNode)
+			this.outerHTML = this.outerHTML
+	}
+	append(element){
+		this.element.appendChild(element)
+		element.outerHTML += ""
+	}
+	appendTo(element){
+		element.appendChild(this.element)
+		this.element.outerHTML += ""
+	}
+	get element(){
+		let element = document.getElementById(this._soumId)
+		if(element)
+			return element
+
+		return this._element
+	}
+	set soumId(soumId){
+		this._soumId = soumId
+		this.element.setAttribute("id", soumId)
+	}
+}
+
+
+class SoumSVG extends SoumElement{
+	constructor(h = "400", w = "400", soumId = Soum.newId()){
+		super("svg", soumId)
+		this.x = 0
+		this.y = 0
+		this.h = h
+		this.w = w
+	}
+	set x(x){
+		this._x = x
+		this.element.setAttribute("viewBox", this.viewBox)
+	}
+	set y(y){
+		this._y = y
+		this.element.setAttribute("viewBox", this.viewBox)
+	}
+	set w(w){
+		this._w = w
+		this.element.setAttribute("viewBox", this.viewBox)
+	}
+	set h(h){
+		this._h = h
+		this.element.setAttribute("viewBox", this.viewBox)
+	}
+	get viewBox(){
+		return `${this._x} ${this._y} ${this._w} ${this._h}`
+	}
+}
+		
+class SoumGraphic extends SoumElement{
+
+	set(name, value){
+		this.element.style.setProperty(name, value)
+	}
+	unset(name){
+		this.element.style.removeProperty(name)
+	}
+	set stroke(stroke){
+		this.set("stroke", stroke)
+	}
+}
+
+
+class SoumCircle extends SoumGraphic{
+
+	constructor(x = 50, y = 50, r = 7, soumId = Soum.newId()){
+		super("circle", soumId)
+		this.x = x
+		this.y = y
+		this.r = r
+	}
+
+	set x(x){
+		this.set("cx", x)
+	}
+	set y(y){
+		this.set("cy", y)
+	}
+	set r(r){
+		this.set("r", r)
+	}
+}
